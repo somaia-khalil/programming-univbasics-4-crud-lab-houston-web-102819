f9ebd88d940bf301ef5ce32ba288827b657998a7
somaia khalil <sumaya_khair@live.com>
2019-10-10 23:35:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,9 +31,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.pop()
 end
 
 def update_element_from_index(array, index_number, element)
-
+array
 end
